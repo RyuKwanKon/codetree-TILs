b8abe8d8be61798fbc4f1b9b8aeb2eab2fec65be
RyuKwanKon <97783148+RyuKwanKon@users.noreply.github.com>
2024-07-07 22:02:09 +0900

    Title: 특정 연산을 이행하는 함수 2, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

--- 240707/특정 연산을 이행하는 함수 2/functions-that-perform-certain-operations-2.java
+++ 240707/특정 연산을 이행하는 함수 2/functions-that-perform-certain-operations-2.java
@@ -2,37 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Number {
-        double value;
-        int index;
-        
-        Number(double value, int index) {
-            this.value = value;
-            this.index = index;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
         
-        List<Number> numbers = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            numbers.add(new Number(Double.parseDouble(st.nextToken()), i));
-        }
+        double num1 = Double.parseDouble(st.nextToken());
+        double num2 = Double.parseDouble(st.nextToken());
+        double num3 = Double.parseDouble(st.nextToken());
 
-        // 가장 큰 수, 중간 수, 가장 작은 수를 각각 구함
-        Number smallest = numbers.stream().min(Comparator.comparingDouble(n -> n.value)).get();
-        Number largest = numbers.stream().max(Comparator.comparingDouble(n -> n.value)).get();
-        Number middle = numbers.stream().filter(n -> n != smallest && n != largest).findFirst().get();
+        // 3개의 숫자를 리스트에 추가
+        List<Double> numbers = new ArrayList<>();
+        numbers.add(num1);
+        numbers.add(num2);
+        numbers.add(num3);
 
-        // 각 수를 조건에 맞게 변환
-        int[] result = new int[3];
-        result[smallest.index] = (int) Math.floor(smallest.value);
-        result[middle.index] = (int) Math.round(middle.value);
-        result[largest.index] = (int) Math.ceil(largest.value);
+        // 정렬
+        Collections.sort(numbers);
+
+        // 각각의 수를 변환
+        int smallest = (int) Math.floor(numbers.get(0));
+        int middle = (int) Math.round(numbers.get(1));
+        int largest = (int) Math.ceil(numbers.get(2));
 
         // 결과 출력
-        System.out.println(result[0] + " " + result[1] + " " + result[2]);
+        System.out.println(largest + " " + smallest + " " + middle);
     }
 }
